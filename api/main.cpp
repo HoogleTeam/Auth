ff94819eec97f868fb1bb7f023fe2d46d544e32f
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-26 06:59:43 -0800

    Update main.cpp

--- api/main.cpp
+++ api/main.cpp
@@ -1,16 +1,17 @@
 //A helpful library for creating a REST API
 #include "crow.h"
-#include <stdio>
+#include <iostream>
 
 using namespace std;
 
 //A function for defining routes
 void addRoute(string route,string function){
-  
+  CROW_ROUTE(app, route)([](){
+    function();
+  });
 }
 
 int main() {
   crow::SimpleApp app;
-  
-  
+   
 }
