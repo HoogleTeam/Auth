58750e7326f69e5d529ef467b1fba115891b4435
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-26 07:19:58 -0800

    Update main.cpp

--- api/main.cpp
+++ api/main.cpp
@@ -1,17 +1,49 @@
-//A helpful library for creating a REST API
-#include "crow.h"
-#include <iostream>
+#include <memory>
+#include <cstdlib>
+#include <restbed>
+#include "auth_functions.h"
 
-using namespace std;
+int main(const int, const char**){
+  
+  //Resources
+  
+  //SignIn
+  auto signIn = make_shared< Resource >();
+  signIn->set_path("/api/signIn");
+  signIn->set_method_handler("GET",sign_in_handler);
+  
+  //SignOut
+  auto signOut = make_shared< Resource >();
+  signOut->set_path("/api/signOut");
+  signOut->set_method_handler("GET",sign_out_handler);
+  
+  //SignUp
+  auto signUp = make_shared< Resource >();
+  signUp->set_path("/api/signUp");
+  signUp->set_method_handler("GET", sign_up_handler);
+  
+  //TokenValidate
+  auto tokenValidate = make_shared< Resource >();
+  tokenValidate->set_path("/api/validateToken");
+  tokenValidate->set_method_handler("GET", token_validate);
+  
+  
+  //Add settings
+  auto settings = make_shared< Settings >( );
+  settings->set_port( 1984 );
+  settings->set_default_header( "Connection", "close" );
 
-//A function for defining routes
-void addRoute(string route,string function){
-  CROW_ROUTE(app, route)([](){
-    function();
-  });
-}
+  
+  //Define the service
+  Service service;
+  //Publish our resources
+  service.publish( signIn );
+  service.publish( signOut );
+  service.publish( signUp );
+  service.publish( tokenValidate );
+  
+  //Start the service
+  service.start( settings );
 
-int main() {
-  crow::SimpleApp app;
-   
+  return EXIT_SUCCESS;
 }
