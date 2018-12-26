da73f9572e78bfc43881200a8dbd8914a937c208
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-26 07:39:46 -0800

    Update auth_functions.h

--- api/auth_functions.h
+++ api/auth_functions.h
@@ -1,1 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+void signIn_handler(const shared_ptr<Session> session) {
+  const auto request = session->get_request( );
+
+  int content_length = request->get_header( "Content-Length", 0 );
+
+  session->fetch( content_length, [ ]( const shared_ptr< Session > session, const Bytes & body )
+  {
+      fprintf( stdout, "%.*s\n", ( int ) body.size( ), body.data( ) );
+      
+      session->close( OK, "Hello, World!", { { "Content-Length", "13" } } );
+  });
+}
+#endif
